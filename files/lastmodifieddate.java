3978afd857d1f764559adedeb76ca661d4e80232
Likhitha101 <21eg106b52@anurag.edu.in>
2025-02-04 14:01:08 +0530

    my commit

---
+++ files/lastmodifieddate.java
@@ -0,0 +1,20 @@
+package com.files;
+import java.io.File;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class lastmodifieddate {
+    public static void main(String[] args) {
+        File file = new File("C:\\test"); 
+
+        if (file.exists()) {
+            long lastModified = file.lastModified();
+            SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
+            System.out.println("Last modified date: " + sdf.format(new Date(lastModified)));
+        } else {
+            System.out.println("File does not exist.");
+        }
+    }
+}
+
+
